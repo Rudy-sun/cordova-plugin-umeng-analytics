2e74d272d7009f41c88ad8b70f4bb77fbd14278d
landys <landys.cn@gmail.com>
2015-04-22 23:05:50 +0800

    rename methods to avoid confliction with other plugins

--- src/ios/AppDelegate+UmengAnalytics.m
+++ src/ios/AppDelegate+UmengAnalytics.m
@@ -13,25 +13,26 @@
 
 // its dangerous to override a method from within a category.
 // Instead we will use method swizzling. we set this up in the load call.
+// Each +load method of each category will be called.
 + (void)load {
     Method original, swizzled;
     
     original = class_getInstanceMethod(self, @selector(init));
-    swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
+    swizzled = class_getInstanceMethod(self, @selector(swizzled_umeng_analytics_init));
     method_exchangeImplementations(original, swizzled);
 }
 
-- (AppDelegate *)swizzled_init {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initQQApp:) name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
+- (AppDelegate *)swizzled_umeng_analytics_init {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initUmengAnalyticsApp:) name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
     
     // This actually calls the original init method over in AppDelegate. Equivilent to calling super
     // on an overrided method, this is not recursive, although it appears that way. neat huh?
-    return [self swizzled_init];
+    return [self swizzled_umeng_analytics_init];
 }
 
 // This code will be called immediately after application:didFinishLaunchingWithOptions:. We need
 // to process notifications in cold-start situations
-- (void)initQQApp:(NSNotification *)notification {
+- (void)initUmengAnalyticsApp:(NSNotification *)notification {
     NSString* appId = [self.viewController.settings objectForKey:@"umengiosappid"];
     if (appId) {
         [MobClick startWithAppkey:appId reportPolicy:BATCH channelId:@"iOS"];
